51623da49f0c83e434245266be28bf53897a8331
kyamashiro <kyamashiro73@gmail.com>
2022-08-01 09:11:46 +0900

    インタラクティブストーリー

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,17 @@
 import '../src/index.css';
+// Registers the msw addon
+import {initialize, mswDecorator} from 'msw-storybook-addon';
+// Initialize MSW
+initialize();
+// Provide the MSW addon decorator globally
+export const decorators = [mswDecorator];
 
 export const parameters = {
-  actions: {argTypesRegex: "^on[A-Z].*"},
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
+    actions: {argTypesRegex: "^on[A-Z].*"},
+    controls: {
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/,
+        },
     },
-  },
 };
